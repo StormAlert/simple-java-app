aee1f2864f423563c5cb2c9f34200fc3ae685f6b
ZiLiang <yzlisme@yahoo.com>
2023-05-31 08:50:42 +0000

    Make a change last

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Today banyfdfdfak rain");
+        System.out.println("Today no rain krappa");
     }
 
     public String getMessage() {
