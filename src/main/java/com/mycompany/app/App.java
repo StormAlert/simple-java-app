be5dc33565818ed6a09c6489fe8cd261a1829cc7
ZiLiang <yzlisme@yahoo.com>
2023-05-31 08:23:13 +0000

    Make a change to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Today banyak rain");
+        System.out.println("Today banyfdfdfak rain");
     }
 
     public String getMessage() {
