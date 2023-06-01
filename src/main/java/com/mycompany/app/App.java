af16686d98aa7c6223ced14326701a6a448e3d5f
ZiLiang <yzlisme@yahoo.com>
2023-06-01 05:29:32 +0000

    truee testnadadadsash

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Today no rain krappa");
+        System.out.println("Today no sdsfsf krappa");
     }
 
     public String getMessage() {
